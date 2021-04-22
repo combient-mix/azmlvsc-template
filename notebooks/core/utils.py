e64b1281ec5d3cb6096e4a5bba1e24f522465023
mcapuccini <marco.capuccini@combient.com>
2021-04-22 12:33:11 +0000

    pipeline refactory

---
+++ notebooks/core/utils.py
@@ -0,0 +1,62 @@
+import shutil
+import tempfile
+from os.path import basename
+
+import joblib
+import pandas as pd
+from azureml.core import Experiment, Run, Workspace
+from azureml.core.model import Model
+from azureml.data.dataset_factory import FileDatasetFactory
+
+def get_ws_and_run(experiment_name): # pylint: disable=unused-variable
+  run = Run.get_context()
+  try:
+    ws = run.experiment.workspace
+  except AttributeError:
+    ws = Workspace.from_config()
+    experiment = Experiment(workspace=ws, name=experiment_name)
+    run = experiment.start_logging()
+  return ws, run
+
+def register_model(run, model, name): # pylint: disable=unused-variable
+  # Dump model
+  tmpdir = tempfile.mkdtemp()
+  model_path = f"{tmpdir}/{name}.joblib"
+  joblib.dump(value=model, filename=model_path)
+
+  # Upload
+  upload_to = f'outputs/{name}.joblib'
+  run.upload_file(name=upload_to, path_or_stream=model_path)
+  shutil.rmtree(tmpdir)
+
+  # Register
+  model = run.register_model(model_name=name, model_path=upload_to)
+  return model
+
+def load_model(ws, name): # pylint: disable=unused-variable
+  # Download model
+  tmpdir = tempfile.mkdtemp()
+  Model(ws, name=name).download(target_dir=tmpdir)
+
+  # Load
+  model = joblib.load(f"{tmpdir}/{name}.joblib")
+
+  # Clean and return
+  shutil.rmtree(tmpdir)
+  return model
+
+def lake_load_csv(datastore, in_path, dtype=None, parse_dates=None): # pylint: disable=unused-variable
+  # Mount directory
+  file_ds = FileDatasetFactory.from_files([(datastore, in_path)])
+  mnt_ctx = file_ds.mount()
+  mnt_ctx.start()
+  mnt_path = mnt_ctx.mount_point
+
+  # Load data
+  pd_df = pd.read_csv(f'{mnt_path}/{basename(in_path)}', dtype=dtype, parse_dates=parse_dates)
+
+  # Stop mount
+  mnt_ctx.stop()
+
+  # Return
+  return pd_df
---
+++ pipelines/aml/core/utils.py
@@ -0,0 +1,50 @@
+import os
+
+import jupytext
+import nbformat as nbf
+from azureml.contrib.notebook import (AzureMLNotebookHandler, NotebookRunConfig, NotebookRunnerStep)
+from azureml.core import Environment, ScriptRunConfig
+
+def nb_runner_step( # pylint: disable=unused-variable
+  notebook_path,
+  ws,
+  target,
+  inputs=None,
+  outputs=None,
+  conda_env_name='default',
+  conda_env_yml='environment.yaml',
+  cell_timeout=1200,
+  **notebook_params,
+):
+  # Convert jupytext to ipynb and write to ntbkstg
+  ntbk = jupytext.read(notebook_path)
+  kernelspec = {"display_name": f"Python [conda env:{conda_env_name}]", "language": "python", "name": conda_env_name}
+  ntbk['metadata']['kernelspec'] = kernelspec
+  ntbkstg_path = os.path.dirname(notebook_path)
+  nb_name = os.path.splitext(os.path.basename(notebook_path))[0]
+  nbf.write(ntbk, f'{ntbkstg_path}/{nb_name}.stg.ipynb')
+
+  # Setup script run configuration
+  compute_target = ws.compute_targets[target]
+  env = Environment.from_conda_specification(conda_env_name, file_path=conda_env_yml)
+  src = ScriptRunConfig(source_directory=ntbkstg_path, compute_target=compute_target, environment=env)
+
+  # Setup notebook run configuration
+  handler = AzureMLNotebookHandler(timeout=cell_timeout)
+  nrc = NotebookRunConfig(source_directory=ntbkstg_path,
+                          notebook=f'{nb_name}.stg.ipynb',
+                          output_notebook=f'outputs/{nb_name}.ipynb',
+                          parameters=notebook_params,
+                          handler=handler,
+                          run_config=src.run_config)
+
+  # Pipeline step
+  step = NotebookRunnerStep(
+    name=nb_name,
+    notebook_run_config=nrc,
+    compute_target=compute_target,
+    allow_reuse=True,
+    inputs=inputs,
+    outputs=outputs,
+  )
+  return step
