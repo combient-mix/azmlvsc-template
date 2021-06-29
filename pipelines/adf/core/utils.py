05e9d00c77d735d9928dfffe69c611ffc4cd069d
mcapuccini <marco.capuccini@combient.com>
2021-06-29 12:35:45 +0000

    add util to create or replace adf trigger

--- pipelines/adf/core/utils.py
+++ pipelines/adf/core/utils.py
@@ -1,3 +1,5 @@
+from itertools import tee
+
 from azureml.pipeline.core import PublishedPipeline
 
 def get_aml_pipe_id(ws, pipe_name): # pylint: disable=unused-variable
@@ -6,3 +8,11 @@ def get_aml_pipe_id(ws, pipe_name): # pylint: disable=unused-variable
     if p.name == pipe_name: # return first one with matching name
       return p.id
   return None
+
+def create_or_replace_trigger(adf_client, ws, adf_name, trigger, trigger_name): # pylint: disable=unused-variable
+  triggers, _ = tee(adf_client.triggers.list_by_factory(ws.resource_group, adf_name))
+  for _, t in enumerate(triggers):
+    if t.name == trigger_name:
+      return
+  adf_client.triggers.create_or_update(ws.resource_group, adf_name, trigger_name, trigger)
+  adf_client.triggers.begin_start(ws.resource_group, adf_name, trigger_name)
